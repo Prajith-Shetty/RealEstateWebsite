8cb9c6ed517fc753b8dd7d588394d4235d522998
Prajith-Shetty <PrajithShettyJ@gmail.com>
2023-11-28 19:16:00 +0530

    Hero section update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      {/* we are enclosing Header and Hero inside a div for getting the white gradient effect */}
+      <div>
+        <div className="white-gradient"/>
+        <Header />
+        <Hero />
+      </div>
     </div>
   );
 }
