18d2be390e394e9a450e1946bcb8edacdbc263f1
Prajith-Shetty <PrajithShettyJ@gmail.com>
2023-12-08 19:11:22 +0530

    Residencies section and slider added

---
+++ src/utils/common.js
@@ -0,0 +1,19 @@
+export const sliderSettings = {
+    slidesPerView : 1,
+    spaceBetween: 50, //50px
+    breakpoints: {
+        480: {
+            slidesPerView: 1
+        },
+        600: {
+            slidesPerView: 2
+        },
+        750: {
+            slidesPerView: 3
+        },
+        1100: {
+            slidesPerView: 4
+        }
+    }
+    //breakpoints is for different screens like 480px
+}
