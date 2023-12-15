634ec6f4de76501631eb7d1da3deb5d9c9bc75c7
Prajith-Shetty <PrajithShettyJ@gmail.com>
2023-12-15 21:13:58 +0530

    minor update

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -13,6 +13,17 @@ import { MdOutlineArrowDropDown } from 'react-icons/md'
 import data from '../../utils/accordion'
 
 const Value = () => {
+
+  const [expandedItems, setExpandedItems] = useState([0]);
+
+  const toggleItem = (index) => {
+    if (expandedItems.includes(index)) {
+      setExpandedItems(expandedItems.filter((item) => item !== index));
+    } else {
+      setExpandedItems([...expandedItems, index]);
+    }
+  };
+  
   return (
     <section className="v-wrapper">
       <div className="paddings innerWidth flexCenter v-container">
@@ -35,11 +46,49 @@ const Value = () => {
           <Accordion
             className="accordion"
             allowMultipleExpanded={false}
+            preExpanded={expandedItems}
+          >
+            {
+              data.map((item, i) => {
+                
+                return (
+                  // to give dynamic classname to accordian item
+                  <AccordionItem className={`accordionItem ${expandedItems.includes(i) ? 'expanded' : 'collapsed'}`} key={i} uuid={i}>
+                    <AccordionItemHeading>
+                      <AccordionItemButton className='flexCenter accordionButton' onClick={() => toggleItem(i)}>
+
+                        <div className="flexCenter icon">
+                          {item.icon}
+                        </div>
+
+                        <span className='primaryText'>{item.heading}</span>
+
+                        <div className="flexCenter icon">
+                          <MdOutlineArrowDropDown size={20} />
+                        </div>
+                      </AccordionItemButton>
+                    </AccordionItemHeading>
+
+                    <AccordionItemPanel>
+                      <p className="secondaryText">{item.detail}</p>
+                    </AccordionItemPanel>
+                  </AccordionItem>
+                )
+              }
+              )
+            }
+          </Accordion>
+          {/* This code maintains the state of expanded accordion items in the expandedItems array and toggles them based on the onClick event of the AccordionItemButton. */}
+
+            {/* //accoring to video
+            <Accordion
+            className="accordion"
+            allowMultipleExpanded={false}
             preExpanded={[0]}
           >
             {
               data.map((item, i) => {
-                const [className,setClassName] = useState(null)
+                const [className, setClassName] = useState(null);
                 return (
                   // to give dynamic classname to accordian item
                   <AccordionItem className={`accordionItem ${className}`} key={i} uuid={i}>
@@ -70,8 +119,7 @@ const Value = () => {
               }
               )
             }
-          </Accordion>
-
+          </Accordion> */}
         </div>
       </div>
     </section>
