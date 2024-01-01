7b1674c490f64461589c1c5aafdb13b01f0fdf32
Prajith-Shetty <PrajithShettyJ@gmail.com>
2024-01-01 20:14:18 +0530

    bookVisit endpoint added to userCtrl

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { createUser } from '../controllers/userCtrl.js';
+import { bookVisit, createUser } from '../controllers/userCtrl.js';
 
 const router = express.Router();
 router.post("/register",createUser);
+router.post("/bookVisit/:id", bookVisit);
 
 export {router as userRoute}
