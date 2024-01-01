bc7ee474ee7b7114aaf3f5972135b63e5ecebfd2
Prajith-Shetty <PrajithShettyJ@gmail.com>
2024-01-01 21:33:28 +0530

    many endpoints added to userCtrl

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { bookVisit, createUser } from '../controllers/userCtrl.js';
+import { bookVisit, cancelBooking, createUser, getAllBookings, getAllFavourites, toFav } from '../controllers/userCtrl.js';
 
 const router = express.Router();
 router.post("/register",createUser);
 router.post("/bookVisit/:id", bookVisit);
+router.get("/allBookings",getAllBookings);
+router.delete("/removeBooking/:id",cancelBooking);
+router.post("/toFav/:rid",toFav);
+router.get("/allFav",getAllFavourites);
 
 export {router as userRoute}
