b7e98ad66c2aa884a1bb6e16b847d11d501a3a21
Prajith-Shetty <PrajithShettyJ@gmail.com>
2024-01-16 19:46:04 +0530

    Property page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import {QueryClient, QueryClientProvider} from 'react-query'
 import {ReactQueryDevtools} from 'react-query/devtools'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Property from './pages/Property/Property';
 
 function App() {
 
@@ -20,7 +21,11 @@ function App() {
       <Route element={<Layout />}> 
       {/* Layout is used since we have header and footer same in all the pages */}
         <Route path='/' element={<Website />} />
-        <Route path='/properties' element={<Properties />}/>
+        <Route path='/properties'>
+          <Route index element={<Properties/>} />
+          {/* index is used if there is no propertyId then simply render the Properties page */}
+          <Route path=':propertyId' element={<Property/>} />
+          </Route>
       </Route>
     </Routes>
     </Suspense>
