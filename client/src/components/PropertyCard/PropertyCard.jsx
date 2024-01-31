88389cd5afae6c72f0cb2fa7d0b4fb6bf33027bc
Prajith-Shetty <PrajithShettyJ@gmail.com>
2024-01-31 16:50:36 +0530

    Like/dislike added

--- client/src/components/PropertyCard/PropertyCard.jsx
+++ client/src/components/PropertyCard/PropertyCard.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './PropertyCard.css'
-import {AiFillHeart} from 'react-icons/ai'
 import {truncate} from 'lodash'
 import { useNavigate } from 'react-router-dom'
+import Heart from '../Heart/Heart'
 
 const PropertyCard = ({card}) => {
     const navigate = useNavigate();
@@ -11,7 +11,7 @@ const PropertyCard = ({card}) => {
         onClick={()=>navigate(`../properties/${card.id}`)}
         // backticks are used since we are using constants 'card.id'
         >
-            <AiFillHeart size={24} color='white'/>
+            <Heart id={card?.id} />
             <img src={card.image} alt="home" />
             <span className='secondaryText r-price'>
                 <span style={{ color: 'orange' }}>$</span>
--- client/src/utils/common.js
+++ client/src/utils/common.js
@@ -16,4 +16,17 @@ export const sliderSettings = {
         }
     }
     //breakpoints is for different screens like 480px
+};
+
+export const updateFavourites = (id, favourites) => {
+    if(favourites.includes(id)){
+        return favourites.filter((resId)=>resId!==id)
+    }
+    else{
+        return [...favourites, id]
+    }
+}
+
+export const checkFavourites = (id, favourites)=> {
+    return favourites.includes(id)? "#fa3e5f" : "white";
 }
